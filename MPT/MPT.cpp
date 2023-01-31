b3d7814ea38c3fa80c7cf99968e4c4689877b0cc
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2023-01-31 13:08:52 +0300

    Variables and data types

---
+++ MPT/MPT.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    system("cls");
+    setlocale(LC_ALL, "Russian_Russia.1251");
+
+    signed int age; // типа данных возраста с модификатором
+    string group, name;
+
+    cout << "Введите ваше ФИО\n";
+    getline(cin, name);
+    cout << "Введите ваш возраст\n";
+    cin >> age;
+    cout << "Введите вашу группу\n";
+    cin >> group;
+    cout << "--------------------------------";
+    cout << "\n";
+    cout << "ФИО - " + name;
+    cout << "\n";
+    cout << "Возраст - " << age;
+    cout << "\n";
+    cout << "Ваша группа - " + group;
+    cout << "\n";
+    cout << "--------------------------------";
+
+}
